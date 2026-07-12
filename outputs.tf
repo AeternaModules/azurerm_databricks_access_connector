4494fa237d3c6a73effe29115073d7f5a764d250
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:09 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "databricks_access_connectors" {
-  description = "All databricks_access_connector resources"
-  value       = azurerm_databricks_access_connector.databricks_access_connectors
-}
 output "databricks_access_connectors_identity" {
-  description = "List of identity values across all databricks_access_connectors"
-  value       = [for k, v in azurerm_databricks_access_connector.databricks_access_connectors : v.identity]
+  description = "Map of identity values across all databricks_access_connectors, keyed the same as var.databricks_access_connectors"
+  value       = { for k, v in azurerm_databricks_access_connector.databricks_access_connectors : k => v.identity }
 }
 output "databricks_access_connectors_location" {
-  description = "List of location values across all databricks_access_connectors"
-  value       = [for k, v in azurerm_databricks_access_connector.databricks_access_connectors : v.location]
+  description = "Map of location values across all databricks_access_connectors, keyed the same as var.databricks_access_connectors"
+  value       = { for k, v in azurerm_databricks_access_connector.databricks_access_connectors : k => v.location }
 }
 output "databricks_access_connectors_name" {
-  description = "List of name values across all databricks_access_connectors"
-  value       = [for k, v in azurerm_databricks_access_connector.databricks_access_connectors : v.name]
+  description = "Map of name values across all databricks_access_connectors, keyed the same as var.databricks_access_connectors"
+  value       = { for k, v in azurerm_databricks_access_connector.databricks_access_connectors : k => v.name }
 }
 output "databricks_access_connectors_resource_group_name" {
-  description = "List of resource_group_name values across all databricks_access_connectors"
-  value       = [for k, v in azurerm_databricks_access_connector.databricks_access_connectors : v.resource_group_name]
+  description = "Map of resource_group_name values across all databricks_access_connectors, keyed the same as var.databricks_access_connectors"
+  value       = { for k, v in azurerm_databricks_access_connector.databricks_access_connectors : k => v.resource_group_name }
 }
 output "databricks_access_connectors_tags" {
-  description = "List of tags values across all databricks_access_connectors"
-  value       = [for k, v in azurerm_databricks_access_connector.databricks_access_connectors : v.tags]
+  description = "Map of tags values across all databricks_access_connectors, keyed the same as var.databricks_access_connectors"
+  value       = { for k, v in azurerm_databricks_access_connector.databricks_access_connectors : k => v.tags }
 }
 
