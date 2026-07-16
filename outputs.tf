9deafe0022df79509807a9a9052cb40e302e3aaa
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:47:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "databricks_access_connectors_id" {
   description = "Map of id values across all databricks_access_connectors, keyed the same as var.databricks_access_connectors"
-  value       = { for k, v in azurerm_databricks_access_connector.databricks_access_connectors : k => v.id }
+  value       = { for k, v in azurerm_databricks_access_connector.databricks_access_connectors : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "databricks_access_connectors_identity" {
   description = "Map of identity values across all databricks_access_connectors, keyed the same as var.databricks_access_connectors"
-  value       = { for k, v in azurerm_databricks_access_connector.databricks_access_connectors : k => v.identity }
+  value       = { for k, v in azurerm_databricks_access_connector.databricks_access_connectors : k => v.identity if v.identity != null && length(v.identity) > 0 }
 }
 output "databricks_access_connectors_location" {
   description = "Map of location values across all databricks_access_connectors, keyed the same as var.databricks_access_connectors"
-  value       = { for k, v in azurerm_databricks_access_connector.databricks_access_connectors : k => v.location }
+  value       = { for k, v in azurerm_databricks_access_connector.databricks_access_connectors : k => v.location if v.location != null && length(v.location) > 0 }
 }
 output "databricks_access_connectors_name" {
   description = "Map of name values across all databricks_access_connectors, keyed the same as var.databricks_access_connectors"
-  value       = { for k, v in azurerm_databricks_access_connector.databricks_access_connectors : k => v.name }
+  value       = { for k, v in azurerm_databricks_access_connector.databricks_access_connectors : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "databricks_access_connectors_resource_group_name" {
   description = "Map of resource_group_name values across all databricks_access_connectors, keyed the same as var.databricks_access_connectors"
-  value       = { for k, v in azurerm_databricks_access_connector.databricks_access_connectors : k => v.resource_group_name }
+  value       = { for k, v in azurerm_databricks_access_connector.databricks_access_connectors : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "databricks_access_connectors_tags" {
   description = "Map of tags values across all databricks_access_connectors, keyed the same as var.databricks_access_connectors"
-  value       = { for k, v in azurerm_databricks_access_connector.databricks_access_connectors : k => v.tags }
+  value       = { for k, v in azurerm_databricks_access_connector.databricks_access_connectors : k => v.tags if v.tags != null && length(v.tags) > 0 }
 }
 
